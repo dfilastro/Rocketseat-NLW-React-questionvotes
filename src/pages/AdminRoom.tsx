9dd0120367dc5b37db72a411c35574eefdd83714
dfilastro <d.filastro@hotmail.com>
2022-01-28 12:52:26 -0300

    delete question button implemented

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,13 +1,15 @@
 import { useParams } from 'react-router-dom';
 
 import logoImg from '../assets/images/logo.svg';
+import deleteImg from '../assets/images/delete.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
-
-import '../styles/room.scss';
 import { Question } from '../components/Question';
 import { useRoom } from '../hooks/useRoom';
+import { database } from '../services/firebase';
+
+import '../styles/room.scss';
 
 type RoomParams = {
   id: string;
@@ -18,6 +20,12 @@ export function AdminRoom() {
   const roomId = params.id;
 
   const { title, questions } = useRoom(roomId!);
+
+  async function handleDeleteQuestion(questionId: string) {
+    if (window.confirm('Are you sure you want to delete this question?')) {
+      const questionRef = await database.ref(`/rooms/${roomId}/questions/${questionId}`).remove();
+    }
+  }
 
   return (
     <div id='page-room'>
@@ -43,8 +51,11 @@ export function AdminRoom() {
                 key={question.id} // algorítmo de reconciliação
                 content={question.content}
                 author={question.author}
-                children={undefined}
-              />
+              >
+                <button type='button' onClick={() => handleDeleteQuestion(question.id)}>
+                  <img src={deleteImg} alt='Remove question' />
+                </button>
+              </Question>
             );
           })}
         </div>
